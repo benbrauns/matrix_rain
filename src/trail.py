0e2c12bd52d9f65e42f75c9d45ec92119e51799d
YangG_Morph <gniayang41@gmail.com>
2022-04-07 19:20:49 -0500

    Optimizations: * Fixed janky trails * Refactor later

--- src/trail.py
+++ src/trail.py
@@ -13,36 +13,37 @@ class Trail(pygame.sprite.Group):
         self.surface = None
         self.rect = None
         self.velocity = pygame.Vector2((0, 0))
+        self.width = 0
+        self.height = 0
         self.new_letters()
 
     def new_letters(self):
         self.letters = make(Letter, self.length, args=(*weighted_random_size(),))
         self.velocity.update(0, self.letters[0].size * SPEED_MULT)
+        self.width = self.letters[0].surface.get_width() * 2
+        self.height = self.letters[0].surface.get_height() * len(self.letters)
         self.update_surface()
 
 
     def update_surface(self):
         window_width = pygame.display.get_window_size()[0]
-        width = self.letters[0].surface.get_width() * 2
-        #print(width)
-        height = self.letters[0].surface.get_height() * len(self.letters)
 
-        self.surface = pygame.Surface((width, height))
+        # Must clear trail surface
+        self.surface = pygame.Surface((self.width, self.height))
         self.surface.set_colorkey('black')
         if not self.rect:
             x = random.randint(0, window_width)
-            y = random.randint(-height - 200, -height)
+            y = random.randint(-self.height - 200, -self.height)
             self.rect = self.surface.get_rect(center=(x, y))
+        # Must redraw every letter
         for letter in self.letters:
             letter.draw(self.surface)
 
     def update(self, dt):
-        # Blit onto Trail surface if new changes
         [letter.update(dt) for letter in self.letters]
         self.rect.center += self.velocity
 
     def draw(self, surface):
-        #[letter.draw(self.surface) for letter in self.letters]
         if any(letter.changed for letter in self.letters):
             self.update_surface()
         surface.blit(self.surface, self.rect)
